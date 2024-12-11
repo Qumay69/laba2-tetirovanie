8fc4c57e1cb8df0943276627ffd2540f33e2f370
Qumay <Qumay@DESKTOP-1CSQH0Q>
2024-12-11 21:55:27 +0200

    Добавьте файлы проекта.

---
+++ laba2 tetirovanie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("X | Y | Z | Expression 1 | Expression 2");
+        for (int X = 0; X <= 1; X++)
+        {
+            for (int Y = 0; Y <= 1; Y++)
+            {
+                for (int Z = 0; Z <= 1; Z++)
+                {
+                    bool expr1 = !(X == 1 && Y == 1) || (X == 1 && Z == 0);
+
+                    bool expr2 = (X == 1 && (Y == 0 || Z == 1)) || Y == 1;
+
+                    Console.WriteLine($"{X} | {Y} | {Z} | {Convert.ToInt32(expr1)} | {Convert.ToInt32(expr2)}");
+                }
+            }
+        }
+    }
+}
